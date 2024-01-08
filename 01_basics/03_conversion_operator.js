c9f61ac977d2877e88fb7574524c3f44318ab456
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2024-01-08 07:03:01 +0000

    conversion of data types

---
+++ 01_basics/03_conversion_operator.js
@@ -0,0 +1,35 @@
+// let score  = "33abc"
+let score  = null
+// let score  = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN
+
+
+// "33" => 33
+// "33abc"  => NaN
+// true => 1;false=>0
+
+
+// let isLoggedIn =1
+let isLoggedIn =""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 = > true;0=>false
+// "" => false
+// "hardik"  =>true
+
+
+let someNumber = 33
+let strignNumbner = String(someNumber)
+
+
+console.log(strignNumbner);
+console.log(typeof strignNumbner);
+
