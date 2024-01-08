2cf8144e0fbede12ebc59244f6395d96806d8597
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2024-01-08 05:56:25 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14453;
+let accountEmail = "hardik@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+let accountState
+
+// accountId =2 //not allowed
+// console.log(accountId);
+
+accountEmail = "sample@accountEmail.com"
+accountPassword = "21212121"
+accountCity = "Bengluru"
+
+
+// console.log([accountId, accountEmail , accountPassword ,accountCity]);
+console.log(accountState);
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"user strict"; //treat all js code as newer version 
+// alert(3+3)// we are using node not browser
+
+
+// console.log(3+3) console.log();//somicolon needed
+
+// number => 2 to powre53
+// bigint
+// string =>""
+// boolean => true/false;
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof( "hardik"))
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
