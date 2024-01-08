ef79ba1baba10a6b909c5cee14bb119b729b0d47
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2024-01-08 07:20:47 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// /* We should avoid using this values  */
+// console.log(null >0);//false
+// console.log(null ==0);//false
+// console.log(null >=0);//true
+
+
+console.log(undefined >0);//false
+console.log(undefined ==0);//false
+console.log(undefined <0);//false
+
+// Strict check
+console.log("2"===2);//false
