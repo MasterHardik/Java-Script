284e7704d7cfa711aa4894fcbfea1047111df866
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2024-01-10 15:19:49 +0000

    function scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -16,3 +16,48 @@ if(true){
 console.log(a);
 // console.log(b);
 console.log(c);
+
+
+function one(){
+    const username = "Hardik"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+
+if(true){
+    const username = "Master"
+    if(username==="Master"){
+        const website = ' Youtube'
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ******** intersting ++++++++++++
+
+addOne(5)///dont give eror
+function addOne(num){
+    return num+1;
+}
+
+addOne(5)
+
+//expressions
+
+// addTwo(5) // gives error because of hositing because we are holding that function : Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)
